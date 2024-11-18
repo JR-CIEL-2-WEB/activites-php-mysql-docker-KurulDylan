986ba0f9ecd743a00563ec0d647996c401ad2ed0
KurulDylan <145962738+KurulDylan@users.noreply.github.com>
2024-11-18 15:55:24 +0000

    Exercice 0.2 fini

--- code/main.php
+++ code/main.php
@@ -8,4 +8,23 @@ echo "La moyenne des notes est : " .moyenne($tab);
 
 echo " la mediane des notes est : " .mediane($tab);
 
+
+
+$salaires = [1500, 4500, 2200, 1500, 3300, 1800, 1700, 2000, 4000];
+
+
+echo "Moyenne des salaires : " . moyenne($salaires) . "</br>";
+echo "Médiane des salaires : " . mediane($salaires) . "</br>";
+
+$salaireNicolas = 2200;
+if ($salaireNicolas > mediane($salaires)) {
+    echo "Nicolas n'est pas dans les moins bien payés de l'entreprise.</br>";
+} else {
+    echo "Nicolas est dans les moins bien payés de l'entreprise.</br>";
+}
+
+
+
+
+
 ?>
