5faaf567f15f259cc4e65be4f8da027bd8161508
KurulDylan <145962738+KurulDylan@users.noreply.github.com>
2024-11-18 16:40:46 +0000

    Avancement tri_selection (Ex4.1)

---
+++ code/tri_selection.php
@@ -0,0 +1,28 @@
+<?php
+
+function tri_selection($tab) {
+    $n = count($tab);
+    for ($i = 0; $i < $n - 1; $i++) {
+        $min = $i;
+        for ($j = $i + 1; $j < $n; $j++) {
+            if ($tab[$j] < $tab[$min]) {
+                $min = $j;
+            }
+        }
+        if ($min != $i) {
+            $memoire = $tab[$i];
+            $tab[$i] = $tab[$min];
+            $tab[$min] = $memoire;
+        }
+    }
+    return $tab;
+}
+
+$tab = [10, 12, 14, 1, 8];
+
+
+echo "tableau trié : " . tri_selection($t) . "</br>";
+echo "Test";
+
+
+?>
